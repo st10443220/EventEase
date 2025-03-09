166164caead6bfa9dde5cc1f15beb5bc638f49a8
st10443220 <st10443220@vcconnect.edu.za>
2025-03-09 15:50:59 +0200

    Add DBContext.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EventEase.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EventEase.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define your DbSets (tables) here, for example:
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
